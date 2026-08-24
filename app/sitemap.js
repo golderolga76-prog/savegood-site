542c719af27c28b364d13c9d03c80078354ff075
golderolga76-prog <golderolga76@gmail.com>
2026-08-24 23:47:17 +0300

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://savegood.store/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
