d6d42ea8f586d64e637677b013cfdf2430fa2171
golderolga76-prog <golderolga76@gmail.com>
2026-08-25 02:00:12 +0300

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,11 @@ export default function sitemap() {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+      url: 'https://savegood.store/video-bot',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
   ];
 }
