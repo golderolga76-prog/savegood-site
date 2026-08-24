6ae940d166508b11986ce300c7e9a32eb08660fd
golderolga76-prog <golderolga76@gmail.com>
2026-08-24 16:05:58 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var script = document.createElement("script");
+                script.async = 1;
+                script.setAttribute("data-cmp-ab","2");
+                script.src = "https://emrldtp.com/NTY2MDg4.js?t=566088";
+                document.head.appendChild(script);
+              })();
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
