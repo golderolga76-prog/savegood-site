6d1638c552a40c480285ab079d6b56ceb40454bd
golderolga76-prog <golderolga76@gmail.com>
2026-09-07 05:59:40 +0300

    Create FlightsPage component for flight search

---
+++ app/flights/page.js
@@ -0,0 +1,8 @@
+export default function FlightsPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Авіаквитки</h1>
+      <p>Тут незабаром з’явиться пошук вигідних авіаквитків.</p>
+    </main>
+  );
+}
