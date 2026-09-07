9cae7277b0caba911085b510fdc4d7bdf7763777
golderolga76-prog <golderolga76@gmail.com>
2026-09-07 22:39:58 +0300

    Remove script tag from RootLayout
    Removed a script tag from the RootLayout component.

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-    <script>(function(s){s.dataset.zone='11748704',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
     <meta name="monetag" content="4770140e8e69c804715245b155eaa84f"/>
     <meta name='impact-site-verification' value='a433a143-8881-4c1f-9905-6d7acc21ef46'/>
         <script
