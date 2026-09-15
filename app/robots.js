b2541fc11aff7a6630dd651973941d4b53a60975
golderolga76-prog <golderolga76@gmail.com>
2026-09-15 09:35:29 +0300

    Allow OAI SearchBot and major search crawlers

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,24 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+    ],
     sitemap: "https://savegood.store/sitemap.xml",
+    host: "https://savegood.store",
   };
 }
